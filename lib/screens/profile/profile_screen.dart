2992bf71a14a76082bbf5a5715a7a095663e4eaf
febieu <febiyantiw96@gmail.com>
2025-05-06 16:45:52 +0700

    add edit profile feature

--- lib/screens/profile/profile_screen.dart
+++ lib/screens/profile/profile_screen.dart
@@ -1,12 +1,215 @@
-import 'package:flutter/cupertino.dart';
+import 'package:flutter/material.dart';
 
-class ProfileScreen extends StatelessWidget {
+class ProfileScreen extends StatefulWidget {
   const ProfileScreen({super.key});
 
   @override
+  State<ProfileScreen> createState() => _ProfileScreenState();
+}
+
+class _ProfileScreenState extends State<ProfileScreen> {
+
+  @override
   Widget build(BuildContext context) {
-    // TODO: implement build
-    throw UnimplementedError();
+    return Scaffold(
+      appBar: AppBar(
+        title: const Text(
+          "Edit Profile",
+          style: TextStyle(
+            fontSize: 18,
+            fontWeight: FontWeight.bold,
+          ),
+        ),
+        backgroundColor: Colors.purple.shade200,
+        elevation: 4,
+        centerTitle: true,
+      ),
+      body: SingleChildScrollView(
+        child: Padding(
+          padding: const EdgeInsets.all(16),
+          child: Column(
+            crossAxisAlignment: CrossAxisAlignment.start,
+            mainAxisAlignment: MainAxisAlignment.start,
+            children: [
+              SizedBox(height: 20),
+              Center(
+                child: Stack(
+                  children: [
+                    CircleAvatar(
+                      radius: 40,
+                      backgroundImage: AssetImage('assets/images/default_avatar.png'),
+                    ),
+                    Positioned(
+                        bottom: 0,
+                        right: 0,
+                        child: CircleAvatar(
+                          radius: 12,
+                          backgroundColor: Colors.purple.shade300,
+                          child: Icon(
+                            Icons.camera_enhance_rounded,
+                            size: 16,
+                            color: Colors.white,
+                          ),
+                        )
+                    )
+                  ],
+                ),
+              ),
+              const SizedBox(height: 30),
+              Text(
+                "Name",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 4),
+              TextField(
+                decoration: InputDecoration(
+                  isDense: true,
+                  enabledBorder: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20),
+                      borderSide: BorderSide(
+                          color: Colors.purple.shade100,
+                          width: 2.0
+                      )
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                      color: Colors.purple.shade200,
+                      width: 2.0,
+                    ),
+                  ),
+                  hintText: "Febiyanti",
+                  hintStyle: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 12),
+              Text(
+                "Email",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 4),
+              TextField(
+                decoration: InputDecoration(
+                  isDense: true,
+                  enabledBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                        color: Colors.purple.shade100,
+                        width: 2.0
+                    ),
+                  ),
+                  focusedBorder: OutlineInputBorder(
+                    borderRadius: BorderRadius.circular(20),
+                    borderSide: BorderSide(
+                      color: Colors.purple.shade200,
+                      width: 2.0,
+                    ),
+                  ),
+
+                  hintText: "febiyanti@outlook.com",
+                  hintStyle: TextStyle(
+                    fontSize: 14,
+                    color: Colors.grey,
+                  ),
+                ),
+              ),
+              const SizedBox(height: 12),
+              Text(
+                "Birth Date",
+                style: TextStyle(
+                  fontWeight: FontWeight.bold,
+                ),
+              ),
+              const SizedBox(height: 8),
+              DatePickerTextField(),
+              const SizedBox(height: 20),
+
+              // Button sumbit
+
+              Center(
+                child: FilledButton(
+                  onPressed: () {
+
+                  },
+                  child: Text(
+                    "Change Profile",
+                    style: TextStyle(
+                      fontWeight: FontWeight.bold,
+                      fontSize: 14,
+                    ),
+                  ),
+                ),
+              )
+            ],
+          ),
+        ),
+      )
+    );
   }
-  
+}
+
+class DatePickerTextField extends StatefulWidget {
+  @override
+  _DatePickerTextFieldState createState() => _DatePickerTextFieldState();
+}
+
+class _DatePickerTextFieldState extends State<DatePickerTextField> {
+  TextEditingController _dateController = TextEditingController();
+
+  Future<void> _selectDate(BuildContext context) async {
+    final DateTime? picked = await showDatePicker(
+      context: context,
+      initialDate: DateTime(2000),
+      firstDate: DateTime(1900),
+      lastDate: DateTime.now(),
+    );
+    if (picked != null) {
+      setState(() {
+        _dateController.text =
+        "${picked.day}/${picked.month}/${picked.year}";
+      });
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return TextField(
+      controller: _dateController,
+      readOnly: true,
+      onTap: () => _selectDate(context),
+      decoration: InputDecoration(
+        isDense: true,
+        hintText: "DD/MM/YYYY",
+        hintStyle: TextStyle(
+          fontSize: 14,
+          color: Colors.grey,
+        ),
+        suffixIcon: Icon(
+            Icons.calendar_today,
+          color: Colors.purple.shade400,
+        ),
+        enabledBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide(
+              color: Colors.purple.shade100,
+              width: 2.0
+          ),
+        ),
+        focusedBorder: OutlineInputBorder(
+          borderRadius: BorderRadius.circular(20),
+          borderSide: BorderSide(
+            color: Colors.purple.shade200,
+            width: 2.0,
+          ),
+        ),
+      ),
+    );
+  }
 }
